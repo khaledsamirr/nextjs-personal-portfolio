bf2ff278d9b7b10df13391de52a313b1f655911d
Khaled Samir <66645843+khaledsamirr@users.noreply.github.com>
2023-10-23 14:11:18 +0300

    Snapping update

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -24,7 +24,7 @@ function Projects({projects}: Props) {
     className='h-screen relative flex overflow-hidden flex-fol text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500'>Projects</h3>
 
-        <div className='relative w-full mt-10 flex overflow-x-scroll overflow-y-hidden snap-x snap-mandtory z-20 scrollbar scrollbar-track-gray-600/20 scrollbar-thumb-purple-800/30'>
+        <div className='relative w-full mt-10 flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-track-gray-600/20 scrollbar-thumb-purple-800/30'>
             {projects?.map((project,i)=>(
                 <div key={project?._id} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-10 md:p-22 h-screen'>
                     
