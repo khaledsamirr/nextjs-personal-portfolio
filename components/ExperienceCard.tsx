b67cd52f2bfc0ef93310cc7fd09377c02821a06c
Khaled Samir <66645843+khaledsamirr@users.noreply.github.com>
2023-10-23 13:11:39 +0300

    Experiences Update

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -9,28 +9,28 @@ type Props = {
 }
 
 function ExperienceCard({experience}: Props) {
-  return <article key={experience._id} className='flex items-center flex-col rounded-lg space-y-4 flex-shrink-0  h-screen[-400px]  w-[400px] lg:w-[600px] md:w-[400px] snap-center bg-[#101010] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
+  return <article key={experience._id} className='flex items-center flex-col rounded-lg space-y-2 flex-shrink-0 w-80 lg:w-[600px] md:w-[400px] snap-center bg-[#101010] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
     <motion.img 
     initial={{y:-100, opacity:0}}
     transition={{duration:1.2}}
     whileInView={{opacity:1,y:0}}
     viewport={{once:true}}
-    className='w-24 h-24 rounded-full xl:w-[120px] xl:h-[120px] object-cover object-center'
+    className='w-16 h-16 rounded-full xl:w-[120px] xl:h-[120px] object-cover object-center'
     src={urlFor(experience?.companyImage).url()} alt="" />
     
-    <div className='px-0 md:px-10'>
-        <h4 className='text-3xl font-light'>{experience?.jobTitle}</h4>
-        <p className='font-bold text-xl mt-1'>{experience?.company}</p>
-        <div className='flex items-center flex-wrap space-x-3 my-2'>
+    <div className='px-0 md:px-2'>
+        <h4 className='text-lg font-light'>{experience?.jobTitle}</h4>
+        <p className='font-bold  mt-1'>{experience?.company}</p>
+        <div className='flex items-center flex-row flex-wrap mt-2'>
           {experience?.technologies.map((tech)=>((
                         <img key={tech?._id} className='h-8 w-8 p-0.5 rounded-full'
                         src={urlFor(tech?.image).url()} alt=''/>
           )))}
 
         </div>
-        <p className='text-sm text-gray-300 py-5'>Started work: {experience?.dataStarted} <br/>Ended: {experience?.isCurrentlyWorkingHere?"Currently working here":experience?.dateEnded} </p>
+        <p className='text-sm text-gray-300 py-2'>Started work: {experience?.dataStarted} <br/>Ended: {experience?.isCurrentlyWorkingHere?"Currently working here":experience?.dateEnded} </p>
 
-        <ul className='list-disc spac-y-4 ml-5 text-xs text-gray-400'>
+        <ul className='list-disc space-y-2 ml-3 text-xs text-gray-400'>
         {experience?.points.map((point)=>((
           <div key={point} className='flex flex-wrap flex-col'><li key={point}>{point}</li></div>
                 
--- components/PersonalSkill.tsx
+++ components/PersonalSkill.tsx
@@ -14,11 +14,12 @@ function PersonalSkill({directionLeft,skill}: Props) {
     <div key={skill?._id}className='group relative flex cursor-pointer'>
         <motion.img
           initial={{
-            x:directionLeft?-200:200,
+            x:directionLeft?-100:100,
             opacity:0
           }}
           transition={{duration:1}}
           whileInView={{opacity:1,x:0}}
+          viewport={{once:true}}
           src={urlFor(skill?.image).url()}
           className='rounded-full border border-gray-500 object-cover  w-16 h-16 xl:w-16 xl:h-16 filter  group-hover:grayscale transition duration-300 ease-in-out'
         
